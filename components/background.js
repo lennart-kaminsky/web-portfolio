43c99ba0bfc1153df6b67421027fb742343a32fc
Lennart Kaminsky <lennartkaminsky@googlemail.com>
2024-04-05 16:26:32 +0200

    remove background svg grain

--- components/background.js
+++ components/background.js
@@ -41,8 +41,8 @@ const BackgroundStyled = styled.div`
       `radial-gradient(circle at 130% ${110 - $circlePosition.line}%, ${
         theme.accentColorPrimary
       } 10%, transparent 30%)`},
-    url("data:image/svg+xml, %3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='4' numOctaves='6' stitchTiles='stitch' /%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.1' /%3E%3C/svg%3E"),
-    ${({ theme }) => theme.bgColorPrimary};
+    /* url("data:image/svg+xml, %3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='4' numOctaves='6' stitchTiles='stitch' /%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.1' /%3E%3C/svg%3E"), */
+      ${({ theme }) => theme.bgColorPrimary};
 
   @media screen and (orientation: landscape) {
     background: ${({ theme, $circlePosition }) =>
@@ -51,7 +51,7 @@ const BackgroundStyled = styled.div`
         `radial-gradient(circle at ${110 - $circlePosition.line}% 130%, ${
           theme.accentColorPrimary
         } 10%, transparent 40%)`},
-      url("data:image/svg+xml, %3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='4' numOctaves='6' stitchTiles='stitch' /%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.1' /%3E%3C/svg%3E"),
-      ${({ theme }) => theme.bgColorPrimary};
+      /* url("data:image/svg+xml, %3Csvg viewBox='0 0 250 250' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='4' numOctaves='6' stitchTiles='stitch' /%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.1' /%3E%3C/svg%3E"), */
+        ${({ theme }) => theme.bgColorPrimary};
   }
 `;
