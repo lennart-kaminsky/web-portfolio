2898b19ea9cbcc160ce1d52ea8ded0d40400a564
Lennart Kaminsky <lennartkaminsky@googlemail.com>
2024-04-08 10:43:58 +0200

    add mail function

--- components/contactSection.js
+++ components/contactSection.js
@@ -1,60 +1,80 @@
 import { HeadlineStyled } from "@/styles/styled";
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function ContactSection() {
+  const [messageSent, setMessageSent] = useState(false);
+  const [sending, setSending] = useState(false);
+
   async function handleSubmit(event) {
     event.preventDefault();
+    setSending(true);
 
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
 
-    // console.log("client form data", data);
-
-    const response = await fetch("/api/contact", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (!response.ok) {
-      console.error(`Error: ${response.status}`);
+    try {
+      const response = await fetch("/api/contact", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        console.error(`Error: ${response.status}`);
+        throw new Error(`response status: ${response.status}`);
+      }
+      setMessageSent(true);
+    } catch (error) {
+      console.error(error);
     }
+    setSending(false);
   }
 
   return (
     <section>
-      <HeadlineStyled>Say Hi</HeadlineStyled>
-      <ContactForm onSubmit={handleSubmit}>
-        <label htmlFor="name">Name </label>
-        <input
-          id="name"
-          name="name"
-          type="text"
-          maxLength="50"
-          placeholder="Jeff Bezos"
-          autoComplete="name"
-          required
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          name="email"
-          type="email"
-          maxLength="60"
-          placeholder="jeffbezos@amazon.com"
-          autoComplete="email"
-          required
-        />
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          name="message"
-          rows="6"
-          placeholder="I wanna hire you!"
-        />
-        <button type="submit">Send Message</button>
-      </ContactForm>
+      <HeadlineStyled>Say hej</HeadlineStyled>
+      <p>
+        If you have any questions, want to get in touch with me or just want to
+        chat about mountain biking, feel free to use the contact form.
+      </p>
+      <p>Alternatively you can write me on LinkedIn or just send a mail to</p>
+      <p>blabla@blablabla.dev</p>
+      {messageSent ? (
+        <p>Message sent!</p>
+      ) : (
+        <ContactForm onSubmit={handleSubmit}>
+          <label htmlFor="name">Name </label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            maxLength="50"
+            placeholder="Jeff Bezos"
+            autoComplete="name"
+            required
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            maxLength="60"
+            placeholder="jeffbezos@amazon.com"
+            autoComplete="email"
+            required
+          />
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            rows="6"
+            placeholder="I wanna hire you!"
+          />
+          <button type="submit">Send Message</button>
+        </ContactForm>
+      )}
     </section>
   );
 }
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,12 +1,41 @@
+import nodemailer from "nodemailer";
+
 export default async function handler(request, response) {
   if (request.method !== "POST") {
     response.status(400).json({ message: "Method not allowed" });
   }
 
+  const transmitter_email = process.env.TRANSMITTER_EMAIL;
+  const transmitter_password = process.env.TRANSMITTER_PASSWORD;
+  const receiver_email = process.env.RECEIVER_EMAIL;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp-mail.outlook.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: transmitter_email,
+      pass: transmitter_password,
+    },
+  });
+
   try {
-    const formData = request.body;
-    // console.log(formData);
-    response.status(201).json({ status: "Message successfully send!" });
+    const data = request.body;
+
+    const info = await transporter.sendMail({
+      from: transmitter_email,
+      to: receiver_email,
+      replyTo: data.email,
+      subject: `Contact via website form from ${data.email}`,
+      html: `<p>Name: ${data.name}</p>
+            <p>Email: ${data.email}</p>
+            <p> ${data.message}</p>`,
+    });
+
+    response.status(201).json({
+      messageId: info.messageId,
+      status: "Message successfully send!",
+    });
   } catch (error) {
     response.status(400).json({ error: error.message });
     console.log(error);
